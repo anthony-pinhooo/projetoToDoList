ecadd7e0b3acdb5e751945e6b8dbef92efb1b365
anthony-pinhooo <contato.anthony27@gmail.com>
2024-01-19 15:54:27 -0400

    created filter component and function of filter to dos

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import Todo from './component/todo';
 import TodoForm from './component/TodoForm';
 import Search from './component/Search';
+import Filter from './component/Filter';
 
 import './App.css'
 
@@ -32,7 +33,11 @@ function App() {
     }
   ])
 
-  const [search, setSearch] = useState("")
+  const [search, setSearch] = useState("");
+
+  const [filter, setFilter] = useState("All");
+
+  const [sort, setSort] = useState("Asc");
 
   const addTodo = (text, category) => {
       const newTodos = [...toDos, {
@@ -72,8 +77,19 @@ function App() {
     <div className='app'>
       <h1>Lista de tarefas</h1>
       <Search search={search} setSearch={setSearch}/>
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort}/>
       <div className='todo-list'>
-        {toDos.filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase())).map((todo) => (
+        {toDos
+          .filter((todo) => filter === "All" 
+            ? true : filter === "Completed" 
+            ? todo.isCompleted 
+            : !todo.isCompleted)
+          .filter((todo) =>
+            todo.text.toLowerCase().includes(search.toLowerCase()))
+          .sort((a, b) => sort === "Asc" 
+            ? a.text.localeCompare(b.text) 
+            : b.text.localeCompare(a.text))
+          .map((todo) => (
           // eslint-disable-next-line react/jsx-key
           <Todo 
             key={todo.id} 
